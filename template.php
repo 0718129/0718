835c5c5c8a86681f8ec404884bae0bac336fd938
christian <0718129@schoolsnet.act.edu.au>
2023-02-10 14:16:04 +1100

    init template

--- template.php
+++ template.php
@@ -2,8 +2,7 @@
 
 function footer():string
 {
-
+    $filename = basename($_SERVER["SCRIPT_FILENAME"]);
+    $footer = "This page was last modified: " . date("F d Y H:i:s.", filemtime($filename));
+    return $footer;
 }
-
-
-?>
