04f74880d4917d2eec237aa0d1153afe95e22bab
christian <0718129@schoolsnet.act.edu.au>
2023-02-20 11:01:46 +1100

    added picture to navbar

--- template.php
+++ template.php
@@ -8,7 +8,7 @@
 </head>
 <nav class="navbar navbar-expand-lg bg-body-tertiary bg-primary" data-bs-theme="dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="#"><img src='https://hips.hearstapps.com/hmg-prod/images/dwayne-johnson-attends-the-jumanji-the-next-level-uk-film-news-photo-1575726701.jpg' width="100"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
