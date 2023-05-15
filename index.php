7b20e9dd5aff6d6e8a74f6f681cf5f4d476e1a89
christian <0718129@schoolsnet.act.edu.au>
2023-05-15 10:54:04 +1000

    added login page

--- index.php
+++ index.php
@@ -1,13 +1,41 @@
 <?php include "template.php" ?>
+<?php include 'login.php'; ?>
 <title>PHP Template</title>
 <body>
 <h1>shoe4cheap</h1>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-6">
+            <p>username: 0718129@schoolsnet.act.edu.au</p>
+            <p>Password: password</p>
+            <p>username: admin@admin.com</p>
+            <p>Password: admin</p>
+        </div>
+        <div class="col-6">
+            <?php if (!isset($_SESSION["EmailAddress"])) : ?>
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+                    <div class="form-group">
+                        <label>Username</label>
+                        <input type="text" name="username" class="form-control" required="required"/>
+                    </div>
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input type="password" name="password" class="form-control" required="required"/>
+                    </div>
 
-Popular Selection
+                    <div class="text-center">
+                        <button name="login" class="btn btn-primary">Login</button>
+                    </div>
+                </form>
+            <?php endif; ?>
+        </div>
+    </div>
+</div>
+
 
 <img src='https://footwearnews.com/wp-content/uploads/2021/08/012234_01-1.jpeg' width="100"></a>
 
-<?php echo footer() ?>
+
 </body>
 <script src="js/bootstrap.bundle.min.js" ></script>
 </html>
